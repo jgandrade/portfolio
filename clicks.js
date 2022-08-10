15ba9679fbff0d168f6cba8870e1ae2160c9eda7
jgandrade1 <johnglenn.andrade@gmail.com>
2022-08-11 00:07:17 +0800

    Finish Responsive of Navbar and add more properties for scss

---
+++ clicks.js
@@ -0,0 +1,27 @@
+let burgerBtn = document.querySelector(".burger-btn");
+let navMenu = document.querySelector(".nav-menu");
+let socialLink = document.querySelector(".socials-links");
+let navBar = document.querySelector(".nav-container");
+
+burgerBtn.addEventListener('click', () => {
+    navMenu.classList.remove('nav-menu-none');
+    navMenu.classList.add('nav-menu-block');
+});
+
+let closeBtn = document.querySelector('.close');
+
+closeBtn.addEventListener('click', () => {
+    navMenu.classList.add('nav-menu-none');
+    navMenu.classList.remove('nav-menu-block');
+    socialLink.classList.remove('socials-links-open');
+    navBar.classList.remove('show-nav');
+});
+
+let showBtn = document.querySelector(".show-btn");
+
+showBtn.addEventListener('click', () => {
+    navMenu.classList.remove('nav-menu-none');
+    navMenu.classList.add('nav-menu-block');
+    socialLink.classList.add('socials-links-open');
+    navBar.classList.add('show-nav');
+});
